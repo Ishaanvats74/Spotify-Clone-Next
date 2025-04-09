ea30d71b81f7a5dd5b01b74fbabfbc27059a41b7
Ishaan vats <Ishaanvats74@gmail.com>
2025-04-10 00:00:34 +0530

    1:03:45 done

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import { LOGIN_URL } from "@/lib/spotify"
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
 
@@ -5,8 +6,9 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization : LOGIN_URL ,
     }),
     // ...add more providers here
   ],
